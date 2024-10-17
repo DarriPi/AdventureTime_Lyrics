356d9a557142b30044670b54871790245861c887
DarriPi <darianpietersen123@gmail.com>
2024-10-17 03:15:45 +0200

    Add project files.

---
+++ Animator.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Just_a_random_for_a_song
+{
+    class TV
+    {
+        // class members
+        Stopwatch watch;
+        List<string> tv;
+        List<string> MessageTv;
+        int Delay = 175;
+
+        // Constructor
+        public TV()
+        {
+            watch = new Stopwatch();
+            MessageTv = ReadTextScenes("Text_Art\\Tv_Static\\Static_Slide_1.txt");
+
+            this.tv = new List<string>();
+            foreach (var item in MessageTv)
+            {
+                this.tv.Add(Regex.Replace(item, "~~~~~~~~~~~~~|#############", "             "));
+            }
+        } //tv
+
+        public void Play() 
+        {
+            int index = 0;
+            while (true)
+            {
+                Console.Clear();
+                // sleep one second
+                
+                if (index < tv.Count)
+                {
+                    Console.WriteLine(tv[index]);
+                }
+                else
+                {
+                    index = 0;
+                    Console.WriteLine(tv[index]);
+                }
+                index++;
+                Thread.Sleep(Delay);
+            } /// animate forever
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="msg">Message must be 13 or less characters long.</param>
+        public void Play(string msg) 
+        {
+            int index = 0;
+            while (true)
+            {
+                Console.Clear();
+                // sleep one second
+                if (index < tv.Count)
+                {
+                    DisplayMessageOnScreen(msg, index);
+                }
+                else
+                {
+                    index = 0;
+                    DisplayMessageOnScreen(msg, index);
+                }
+                index++;
+                Thread.Sleep(Delay);
+            } /// animate forever
+        }
+        // helper method for playing a message
+        private void DisplayMessageOnScreen(string msg, int index) 
+        {
+            msg = CheckString(msg);
+            msg = InsertTVOne(msg, index);
+            msg = InsertTVTwo("             ", msg, index);
+            Console.WriteLine(msg);
+        } // display msg on screen
+
+        // play for two lines
+        public void Play(string line1, string line2, int time) 
+        {
+            int index = 0;
+
+            watch.Start();
+            while (watch.ElapsedMilliseconds < time)
+            {
+                Console.Clear();
+                // sleep one second
+                if (index < tv.Count)
+                {
+                    DisplayOnBothLines(line1, line2, index);
+                }
+                else
+                {
+                    index = 0;
+                    DisplayOnBothLines(line1, line2, index);
+                }
+                index++;
+                Thread.Sleep(Delay);
+            } /// animate forever
+            watch.Stop();
+            watch.Reset();
+        } // write on both lines
+        private void DisplayOnBothLines(string msg1, string msg2, int index) 
+        {
+            string result = "";
+            msg1 = CheckString(msg1);
+            msg2 = CheckString(msg2);
+            result = InsertTVOne(msg1, index);
+            result = InsertTVTwo(msg2, result, index);
+            Console.WriteLine(result);
+        } // will display on both lines
+
+        /// <summary>
+        /// Will allow you to play lines and lines of songs using 
+        /// a list of tuples Item1, is the line and item2 the time to display
+        /// </summary>
+        /// <param name="lines">Provide a lit of tuples with the line and length to play in miliseconds</param>
+        public void Play(List<Tuple<string, string, int>> lines) 
+        {
+            int index = 0;
+            int linesIndex = 0;
+            while (linesIndex < lines.Count)
+            {
+                watch.Start();
+                Console.Clear();
+                // sleep one second
+                if (watch.ElapsedMilliseconds < lines[linesIndex].Item3)
+                {
+                    if (index < tv.Count)
+                    {
+                        DisplayOnBothLines(lines[linesIndex].Item1, lines[linesIndex].Item2, index);
+                    }
+                    else
+                    {
+                        index = 0;
+                        DisplayOnBothLines(lines[linesIndex].Item1, lines[linesIndex].Item2, index);
+                    }
+                    index++;
+                } // only display and increment if the time elapsed
+                else
+                {
+                    if (index < tv.Count)
+                    {
+                        DisplayOnBothLines(lines[linesIndex].Item1, lines[linesIndex].Item2, index);
+                    }
+                    else
+                    {
+                        index = 0;
+                        DisplayOnBothLines(lines[linesIndex].Item1, lines[linesIndex].Item2, index);
+                    }
+                    watch.Stop();
+                    watch.Reset();
+                    linesIndex++;
+                    index++;
+                }  
+                Thread.Sleep(Delay);
+            } /// animate forever
+        } // will play with the message 
+
+
+        // will simply set display time
+        public void SetDelayTime(int time) 
+        {
+            Delay = time;
+        } // set delay time
+
+        // private helper methods
+        private string InsertTVTwo(string msg, string tv, int index)
+        {
+            string result = tv;
+            result = Regex.Replace(result, "#############", msg);
+            return result;
+        }//  will insert the messages in the right
+        private string InsertTVOne(string msg, int index) 
+        {
+            string result = MessageTv[index];
+            result = Regex.Replace(result, "~~~~~~~~~~~~~", msg);
+            return result;
+        }//  will insert the messages in the right
+        private string CheckString(string msg) 
+        {
+            if (msg.Length == 13)
+            {
+                return msg;
+            } // return if correct length
+
+            if (msg.Length > 13) 
+            {
+                return "Invalid...";
+            } // invalid string
+
+            int count = msg.Length;
+            while (count < 13)
+            {
+                msg += " ";
+                count++;
+            } // add the other characters to fulfill 13 rule
+
+            return msg;
+        }// make sure it has 13 characters
+        private List<string> ReadTextScenes(string path)
+        {
+            List<string> result = new List<string>();
+            string text = "";
+            foreach (string line in File.ReadLines(path))
+            {
+                if (line != "===")
+                {
+                    text += "\n" + line;
+                }// then add line
+                else
+                {
+                    result.Add(text);
+                    text = "";
+                } // add to the list
+            } // read lines
+            return result;
+        }
+    } // will simply loop through a string of 
+}
---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+using MusicPlayerLib;
+
+namespace Just_a_random_for_a_song
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            string path = "Text_Art\\Tv_Static\\Static_Slide_1.txt";
+
+
+            // make tv and play
+            MusicPlayer player = new MusicPlayer();
+            TV tv = new TV();
+
+            // Display the title
+            //tv.Play("Adventure", "Time", 2);
+
+            player.Play("Music/Island Song (Come Along with Me) (feat. Ashley Eriksson).mp3");
+            tv.SetDelayTime(150);
+            tv.Play(MakeLines());
+            player.Stop();
+            Console.ReadKey();
+        }
+        //static List<string> ReadTextScenes(string path) 
+        //{
+        //    List<string> result = new List<string>();
+        //    string text = "";
+        //    foreach (string line in File.ReadLines(path))
+        //    {
+        //        if (line != "===")
+        //        {
+        //            text += "\n" + line;
+        //        }// then add line
+        //        else
+        //        {
+        //            result.Add(text);
+        //            text = "";
+        //        } // add to the list
+        //    } // read lines
+        //    return result;
+        //}
+
+        private static List<Tuple<string, string, int>> MakeLines() 
+        {
+            List<Tuple<string, string, int>> result = new List<Tuple<string, string, int>>();
+
+            result.Add(new Tuple<string, string, int>("Come along", "with me", 4500));
+            result.Add(new Tuple<string, string, int>("And the", "butterflies", 2500));
+            result.Add(new Tuple<string, string, int>("and BEES", "<(||)''", 2500));
+            result.Add(new Tuple<string, string, int>("We can wander", "through the", 2000));
+            result.Add(new Tuple<string, string, int>("   FOREST", "*************", 1750));
+            result.Add(new Tuple<string, string, int>("And do so", "as we please", 3500));
+            result.Add(new Tuple<string, string, int>("Come along", "with me", 4000));
+            result.Add(new Tuple<string, string, int>("To a cliff", "under a tree", 3500));
+            result.Add(new Tuple<string, string, int>("Where we can", "gaze upon", 2000));
+            result.Add(new Tuple<string, string, int>("THE WATER", "~~~~~~~~~~~~~", 2000));
+            result.Add(new Tuple<string, string, int>("As an", "everlasting", 1750));
+            result.Add(new Tuple<string, string, int>("   DREAM", "*.*.*.*.*.*.*", 3000));
+            result.Add(new Tuple<string, string, int>("All of my", "collections", 3000));
+            result.Add(new Tuple<string, string, int>("I'll share", "them all", 2000));
+            result.Add(new Tuple<string, string, int>("   WITH YOU", "<3<3<3", 2200));
+            result.Add(new Tuple<string, string, int>("Maybe by", "next summer", 3000));
+            result.Add(new Tuple<string, string, int>("We won't", "have changed", 3000));
+            result.Add(new Tuple<string, string, int>("  OUR TUNES", "\\(OcO)/", 2000));
+            result.Add(new Tuple<string, string, int>("We'll still", "want to be", 3000));
+            result.Add(new Tuple<string, string, int>("With the", "butterflies", 2500));
+            result.Add(new Tuple<string, string, int>("and BEES", "<(||)''", 1500));
+            result.Add(new Tuple<string, string, int>("Making up", "new numbers", 3000));
+            result.Add(new Tuple<string, string, int>("And living", "so merrily", 4500));
+            result.Add(new Tuple<string, string, int>("All of my", "collections", 4500));
+            result.Add(new Tuple<string, string, int>("I'll share", "them all", 2750));
+            result.Add(new Tuple<string, string, int>("  WITH YOU", "<3<3<3)", 1500));
+            result.Add(new Tuple<string, string, int>("I'll be here", "for you", 2750));
+            result.Add(new Tuple<string, string, int>("ALWAYS", "(INFINITY)", 2000));
+            result.Add(new Tuple<string, string, int>("And always", "be with you", 4000));
+            result.Add(new Tuple<string, string, int>("Come along", "with me", 3500));
+            result.Add(new Tuple<string, string, int>("And the", "butterflies", 1700));
+            result.Add(new Tuple<string, string, int>("and BEES", "<(||)''", 1700));
+            result.Add(new Tuple<string, string, int>("We can wander", "through the", 1700));
+            result.Add(new Tuple<string, string, int>("  FOREST", "*************", 1700));
+            result.Add(new Tuple<string, string, int>("And do so", "as we please", 2000));
+            result.Add(new Tuple<string, string, int>("And living", "so merrily", 10000));
+
+            return result;
+        } // will add the lines of the song I wish to play
+        private void RepeatChorus() 
+        {
+        }
+    }
+}
